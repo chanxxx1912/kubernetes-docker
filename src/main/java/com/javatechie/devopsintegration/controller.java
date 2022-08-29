de4e925a463c529a253e05ef0b2bb8c0bc418fd7
chanxxx1912 <chandana9325@gmail.com>
2022-08-29 21:25:07 +0530

    first commit

---
+++ src/main/java/com/javatechie/devopsintegration/controller.java
@@ -0,0 +1,15 @@
+package com.javatechie.devopsintegration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+	@GetMapping(path = "/")
+	public String hello() {
+		
+		return "Hello World from kubernetes";
+	}
+	
+}
